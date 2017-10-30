b48fc0c6da1c49ab56d799bb8f38dfc033bd9894
COMPUTER\MSI <taijf777@gmail.com>
2017-10-31 07:46:25 +0800

    Commit 14

--- FYP Prototype/Assets/LobbyController.cs
+++ FYP Prototype/Assets/LobbyController.cs
@@ -18,9 +18,12 @@ public class LobbyController : NetworkManager {
 	public Transform uiPlayer1;
 	public Transform uiPlayer2;
 	public GameObject uiWaiting;
+	public GameObject lobbyCanvas;
+
 	[Space]
 	public GameObject player1;
 	public GameObject player2;
+	public int readyPlayer;
 	// Use this for initialization
 	void Start () {
 		s_Singleton = this;
@@ -42,6 +45,7 @@ public class LobbyController : NetworkManager {
 	public void startClient(){
 		networkDiscovery.StartAsClient ();
 		changeTo (FindMatchPanel);
+		uiWaiting.SetActive (false);
 	}
 
 	public void changeTo(RectTransform newPanel){
@@ -59,12 +63,21 @@ public class LobbyController : NetworkManager {
 	}
 
 	public override void OnClientConnect(NetworkConnection conn){
+		Debug.Log ("OnClientConnect");
 		ClientScene.Ready (conn);
-		ClientScene.AddPlayer (conn, (short)numPlayers);
+		ClientScene.AddPlayer (conn, 0);
 		if (numPlayers > 1) {
 			uiWaiting.SetActive (false);
 		}
-		Debug.Log (numPlayers);
+		Debug.Log (base.numPlayers);
+	}
+
+	public override void OnServerConnect(NetworkConnection conn){
+		Debug.Log ("OnServerConnect");
+		if (numPlayers > 0) {
+			uiWaiting.SetActive (false);
+		}
+		Debug.Log (base.numPlayers);
 	}
 
 	public void OnPlayerDisconnect(NetworkPlayer id){
@@ -72,16 +85,45 @@ public class LobbyController : NetworkManager {
 		Network.DestroyPlayerObjects (id);
 	}
 
-	public override void OnServerAddPlayer (NetworkConnection conn, short playerControllerId)
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId, NetworkReader extraMessageReader)
 	{
-		GameObject player;
-		Debug.Log ("playerControllerId = " + playerControllerId);
-		if (uiPlayer1.GetComponentInChildren<Button> () == null) {
-			player = (GameObject)Instantiate (player1, uiPlayer1.transform, false);
-		} else {
-			player = (GameObject)Instantiate (player2, uiPlayer2.transform, false);
+		OnServerAddPlayerInternal(conn, playerControllerId);
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		OnServerAddPlayerInternal(conn, playerControllerId);
+	}
+
+	void OnServerAddPlayerInternal(NetworkConnection conn, short playerControllerId)
+	{
+		print ("OnServerAddPlayerInternal");
+		if (playerControllerId < conn.playerControllers.Count  && conn.playerControllers[playerControllerId].IsValid && conn.playerControllers[playerControllerId].gameObject != null)
+		{
+			if (LogFilter.logError) { Debug.LogError("There is already a player at that playerControllerId for this connections."); }
+			return;
 		}
-		NetworkServer.Spawn (player);
-		NetworkServer.AddPlayerForConnection (conn, player, playerControllerId);
+
+		GameObject player;
+		Transform startPos = GetStartPosition();
+		if (uiPlayer1.GetComponentInChildren<NetworkIdentity> () == null) {
+			player = (GameObject)Instantiate (player1,Vector3.zero, Quaternion.identity);
+		} else {
+			player = (GameObject)Instantiate (player2,Vector3.zero, Quaternion.identity);
+		}
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+
+	public void PlayerReady(){
+		readyPlayer++;
+		if (readyPlayer > 1) {
+			print ("All Ready");
+			base.ServerChangeScene ("Main02");
+		}
+	}
+
+	public void PlayerUnready(){
+		readyPlayer--;
 	}
 }
